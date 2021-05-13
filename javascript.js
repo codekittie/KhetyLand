5620e1f907cf4ae114af179befc16619f82cec46
codekittie <khetyaviles@gmail.com>
2021-05-13 12:41:10 -0600

    first commit

---
+++ javascript.js
@@ -0,0 +1,6 @@
+function myFunction() {
+    document.getElementById("panel").style.display = "block";
+  }
+function myFunction2() {
+    document.getElementById("panel").style.display = "none";
+  }
