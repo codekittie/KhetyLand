d8531809d2fd8686f76e4f11626c4c3fa6b138cc
codekittie <khetyaviles@gmail.com>
2021-05-19 18:30:41 -0600

    Next blog post

--- javascript.js
+++ javascript.js
@@ -4,3 +4,9 @@ function myFunction() {
 function myFunction2() {
     document.getElementById("panel").style.display = "none";
   }
+  function myFunction3() {
+    document.getElementById("panel").style.display = "block";
+  }
+function myFunction4() {
+    document.getElementById("panel").style.display = "none";
+  }
